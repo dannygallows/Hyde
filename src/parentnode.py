033dca493a53ab32649ed20fbab4ef09a67aa89c
dannygallows <43937866+dannygallows@users.noreply.github.com>
2026-02-23 20:01:17 +0100

    Add ParentNode class + unit tests

---
+++ src/parentnode.py
@@ -0,0 +1,19 @@
+from htmlnode import HTMLNode
+from leafnode import LeafNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("Error: Parent nodes must have a tag.")
+        if not self.children:
+            raise ValueError("Error: Children nodes must have a value.")
+        
+        children_html = ""
+        
+        for child in self.children:
+            children_html += child.to_html()
+        
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>" 
