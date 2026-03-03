1895f86abeb7f372d7e3e4dff62b61a4060e8383
dannygallows <43937866+dannygallows@users.noreply.github.com>
2026-03-03 20:10:21 +0100

    Add node splitting function for markdown images + unit test

--- src/split_nodes_image_link.py
+++ src/split_nodes_image_link.py
@@ -1,36 +1,11 @@
-from textnode import TextType, TextNode
 from extract_markdown import extract_markdown_images, extract_markdown_links
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    
-    new_nodes = []
-    
-    for old_node in old_nodes:
-        if old_node.text_type != TextType.TEXT:
-            new_nodes.append(old_node)
-            continue
-        
-        split_nodes = old_node.text.split(delimiter)    
-        
-        if len(split_nodes) % 2 == 0:
-            raise Exception("ERROR: Invalid Markdown syntax! Closing delimiter not found.")
-        
-        for i in range(len(split_nodes)):
-            if split_nodes[i] == "":
-                continue
-            if i % 2 == 0:
-                new_nodes.append(TextNode(split_nodes[i], TextType.TEXT))
-            else:
-                new_nodes.append(TextNode(split_nodes[i], text_type))
-    
-    return new_nodes
-
 def split_nodes_image(old_nodes):
-    
+
     new_nodes = []
-    
+
     for old_node in old_nodes:
-        
+
         if old_node.text_type != TextType.TEXT:
             new_nodes.append(old_node)
             continue
@@ -45,26 +20,23 @@ def split_nodes_image(old_nodes):
         if not images:
             new_nodes.append(old_node)
             continue
-        
+
         for image in images:
             image_alt = image[0]
             image_link = image[1]
             split_nodes = original_text.split(f"![{image_alt}]({image_link})", 1)
-            
+
             if len(split_nodes) % 2 == 0:
                 raise Exception("ERROR: Invalid Markdown syntax! valid syntax: ![image_alt](image_link)")
 
             if split_nodes[0] != "":
                 new_nodes.append(TextNode(split_nodes[0], TextType.TEXT))
-            
+
             new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
-            
+
             original_text = split_nodes[1]
-            
+
         if original_text != "":
             new_nodes.append(TextNode(original_text, TextType.TEXT))
-            
+
     return new_nodes
-
-
-
