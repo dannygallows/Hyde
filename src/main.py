4ca2452e3a042e1631706fab22bf6dd0233aff24
dannygallows <43937866+dannygallows@users.noreply.github.com>
2026-03-08 12:16:52 +0100

    Function to copy static/ to public/ folder

--- src/main.py
+++ src/main.py
@@ -1,11 +1,26 @@
-from textnode import TextNode, TextType
-from htmlnode import HTMLNode
-from leafnode import LeafNode
+import os
+import shutil
 
 def main():
-    text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(text_node)
+    copy_static_to_public()
+ 
+def copy_static_to_public(src="./static", dst = "./public"):
+    # lets do rm -rf public and cp -r static public with extra steps! (to stay platform independent!)
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    os.mkdir(dst)
+
+    static_files = os.listdir(src)
+
+    for file in static_files:
+        src_path = os.path.join(src, file)
+        dst_path = os.path.join(dst, file)
+
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dst_path)
+        else:
+            copy_static_to_public(src_path, dst_path)
+
 
 if __name__ == "__main__":
     main()
-    
