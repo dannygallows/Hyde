25db3312f21c8b2491884b1f759cbf00fa5010df
dannygallows <43937866+dannygallows@users.noreply.github.com>
2026-03-09 21:21:29 +0100

    Build and deploy the application to GitHub Pages

--- src/main.py
+++ src/main.py
@@ -1,18 +1,23 @@
 import os
 import shutil
+import sys
 from markdown_to_html_node import markdown_to_html_node
 from extract_markdown import extract_title
 
 
 def main():
-    if os.path.exists("./public"):
-        shutil.rmtree("./public")
-    copy_static_to_public("./static", "./public")
-    #generate_page("content/index.md", "template.html", "public/index.html")
-    generate_pages_recursive("content", "template.html", "./public")
+    if os.path.exists("./docs"):
+        shutil.rmtree("./docs")
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+    copy_static_to_docs("./static", "./docs")
+    #generate_page("content/index.md", "template.html", "docs/index.html")
+    generate_pages_recursive("content", "template.html", "./docs", basepath)
 
 
-def copy_static_to_public(src, dst):
+def copy_static_to_docs(src, dst):
     os.mkdir(dst)
     static_files = os.listdir(src)
     for file in static_files:
@@ -21,7 +26,7 @@ def copy_static_to_public(src, dst):
         if os.path.isfile(src_path):
             shutil.copy(src_path, dst_path)
         else:
-            copy_static_to_public(src_path, dst_path)
+            copy_static_to_docs(src_path, dst_path)
 
 
 def generate_page(from_path, template_path, dest_path):
@@ -39,7 +44,7 @@ def generate_page(from_path, template_path, dest_path):
 
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     os.makedirs(dest_dir_path, exist_ok=True)
     content_files = os.listdir(dir_path_content)
     for file in content_files:
@@ -55,10 +60,11 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
                 md_to_html = markdown_to_html_node(markdown).to_html()
                 page_title = extract_title(markdown)
                 html_page = template.replace("{{ Title }}", page_title).replace("{{ Content }}", md_to_html)
+                html_page = html_page.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
                 with open(dest_path, "w") as f:
                     f.write(html_page)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 
 if __name__ == "__main__":
